36c6739eeca61956b21c442b7966602141fa2ae5
Pranik Chainani <pranikc@gmail.com>
2021-06-23 14:29:43 -0400

    commit tree 412944461cb410f86375fbea5f1398a66316f71d

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,26 @@
 # Add your code here
+class Dog
+    attr_reader :name
+    @@all = []
+
+    def initialize (name)
+        @name = name
+        self.save
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.clear_all
+        @@all.clear
+    end
+
+    def self.print_all
+        puts @@all.map{|each_dog| each_dog.name}
+    end
+
+    def save
+        @@all << self
+    end
+end
